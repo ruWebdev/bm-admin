e99c37304dca6b942150b16ca3c26a375bb6a04e
Nikolay I. Dolzhnikov <n.dolzhnikov@gmail.com>
2024-04-17 10:03:01 +0300

    Миграция

---
+++ database/migrations/2024_04_11_143005_create_composer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('composers', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('page_alias');
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('birth_date');
+            $table->string('death_date');
+            $table->string('main_photo');
+            $table->string('short_description');
+            $table->text('long_description');
+            $table->boolean('enabled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('composer');
+    }
+};
